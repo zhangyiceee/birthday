e39dc81336aa8d9a00374459c95f2b609893a4af
张 <zhangyiceee@163.com>
2020-08-30 12:00:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ var config = {
      */
     imgs: {
         "心爱的小可爱": "./imgs/xiaokeai.png",
-        "今天是你的生日": "./imgs/birthday.jpg",
+        "今天是你的生日": "./imgs/qqs.png",
         "咱俩一起上桥":"./imgs/qiao.png",
         "一起合影":"./imgs/hz.png",
         "你住院了":"./imgs/yk1.png",
@@ -32,8 +32,8 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "来点音乐",
+        bannar_coming: "少点颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
